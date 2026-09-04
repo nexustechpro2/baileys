d17dec4c3304fe9a1379154aade683dbc9933a0a
paulfarayola2006 <nexustechpro2@gmail.com>
2026-09-04 13:27:10 +0100

    refactor(WAProto/index): simplify init flow with top-level await and background sync using _reloadProto

--- WAProto/index.js
+++ WAProto/index.js
@@ -1,79 +1,33 @@
-import { existsSync } from 'fs'
-import { fileURLToPath } from 'url'
-import { join, dirname } from 'path'
-import { fetchProtoBundle, getWAVersion as fetchVersion } from './fetcher.js'
+import { fetchProtoBundle } from './fetcher.js'
 import { parseAndWriteProto } from './parser.js'
-import { generateTable } from './WAProtoCompile.js'
+import { generateTable, _reloadProto } from './WAProtoCompile.js'
+import { fileURLToPath } from 'url'
+import { existsSync } from 'fs'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const PROTO_FILE = join(__dirname, 'WAProto.proto')
-const TABLE_FILE = join(__dirname, 'WAProto.json')
+const PROTO_FILE = fileURLToPath(new URL('./WAProto.proto', import.meta.url))
+const TABLE_FILE = fileURLToPath(new URL('./WAProto.json', import.meta.url))
 
-let _proto = null
-let _codec = null
-let _version = null
-let _initPromise = null
+// First run: block until proto and table are generated
+if (!existsSync(PROTO_FILE) || !existsSync(TABLE_FILE)) {
+    const { bundle, version } = await fetchProtoBundle()
+    if (bundle) {
+        await parseAndWriteProto(bundle, version)
+        await generateTable(PROTO_FILE, TABLE_FILE)
+        _reloadProto()
+    }
+}
 
-async function initialize() {
-    const protoExists = existsSync(PROTO_FILE)
-    let version = null
-
+// Background: keep proto in sync with WA Web updates
+; (async () => {
     try {
-        const { changed, bundle, version: v } = await fetchProtoBundle()
-        version = v
-
+        const { changed, bundle, version } = await fetchProtoBundle()
         if (changed && bundle) {
             await parseAndWriteProto(bundle, version)
             await generateTable(PROTO_FILE, TABLE_FILE)
+            _reloadProto()
         }
-    } catch (err) {
-        if (!protoExists) throw err
-    }
+    } catch { }
+})()
 
-    if (!existsSync(PROTO_FILE)) {
-        throw new Error('[WAProto] WAProto.proto does not exist and could not be generated')
-    }
-
-    if (!existsSync(TABLE_FILE)) {
-        await generateTable(PROTO_FILE, TABLE_FILE)
-    }
-
-    const { proto: builtProto, codec: builtCodec } = await import('./WAProtoCompile.js?t=' + Date.now())
-    _proto = builtProto
-    _codec = builtCodec
-
-    if (version) {
-        _version = [2, 3000, parseInt(version, 10)]
-    } else {
-        try {
-            const v = await fetchVersion()
-            _version = [2, 3000, parseInt(v, 10)]
-        } catch {
-            _version = [2, 3000, 0]
-        }
-    }
-}
-
-async function initProto() {
-    if (_proto) return
-    if (_initPromise) { await _initPromise; return }
-    _initPromise = initialize()
-    await _initPromise
-}
-
-await initProto()
-
-export { _proto as proto, _codec as codec }
-
-export function getWAVersion() {
-    if (!_version) throw new Error('[WAProto] Not initialized')
-    return _version
-}
-
-export default new Proxy({}, {
-    get(_, prop) {
-        if (!_proto) throw new Error('[WAProto] Not initialized')
-        if (prop === 'proto') return _proto
-        return _proto[prop]
-    },
-})
+export { getWAVersion } from './fetcher.js'
+export { proto, codec, generateTable, proto as default } from './WAProtoCompile.js'
